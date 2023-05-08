b98a8c37e5391a8bbfd766d6c19772ee6fe9c298
EugenioBugli <eugeniobugli15@gmail.com>
2023-05-08 23:28:36 +0200

    home improvements

--- 2.0/index.php
+++ 2.0/index.php
@@ -8,24 +8,94 @@
     <title>Reader's List</title>
     <style>
         .home-text {
-            background-image: linear-gradient(to top, rgba(7, 70, 33,0.2), rgba(7, 70, 33,1));
+            background-image: linear-gradient(to top, rgba(7, 70, 33,0), rgba(7, 70, 33,1));
+            /*background-color: rgba(7,70,33,0.8);*/
             box-shadow: 0 0 10px;
             overflow: hidden;
-            width: 100vw;
-            height: 100vh;
-            
+            width: 60vw;
+            height: 60vh;
+            grid-template: 
+            'a a'
+            'b c'
+            'd e';
+            display: grid;
+            grid-template: auto;
+        }
+        
+        .title-desc {
+            font-weight: 1000;
+            font-size: 40px;
+            grid-area: a;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top: auto;
+            margin-bottom: auto;
         }
 
         h2 {
             color: blanchedalmond;
             font-size: 25px;
-            font-weight: 1000;
+            font-weight: 800;
             text-align: center;
         }
 
+        .text1 {
+            grid-area: c;
+            background-color: blue;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top: auto;
+            margin-bottom: auto;
+            white-space: initial;
+        }
+
+        .text2 {
+            grid-area: d;
+            background-color: blue;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top: auto;
+            margin-bottom: auto;
+            white-space: initial;
+        }
+
         img {
-            width: 140px;
-            height: 140px;
+            width: 240px;
+            height: 240px;
+        }
+
+        .gif {
+            grid-area: e;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top: auto;
+            margin-bottom: auto;
+        }
+
+        figure {
+            grid-area: b;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top: auto;
+            margin-bottom: auto;
+        }
+
+        blockquote {
+            margin: 0;
+        }
+
+        blockquote q {
+            padding: 40px;
+            background: rgb(47, 79, 79);
+            color: blanchedalmond;
+            border-radius: 5px;
+            font-size: 20px;
+            font-weight: 700;
+        }
+
+        figcaption {
+            color: blanchedalmond;
+            right:0;
         }
         
     </style>
@@ -34,22 +104,33 @@
 <body>
     <?php include("navbar.php"); ?>
     <div class="home-text">
-        <h2>
-            Questo sito web permette di creare una Reading List.
-            <br>
-            Creare una reading list ti permette di salvare tutti i tuoi libri e di tenere traccia di ogni lettura che stai affrontando.
-            Ogni utente ha la possibilità di salvare tutti i libri che è interessato a leggere ed è in grado di salvare i propri progressi
-            riguardanti le pagine lette.
-            <br>
+        <h2 class="title-desc">
+            Questo sito web permette di creare una Reading List
         </h2>
-        <img src="https://7551bdfc54adf45425bb-e1819ba959867bdb3382b3652a5f5ff1.ssl.cf5.rackcdn.com/animations/467/1435256441-Young_animation_education062515_02.gif">
-        <img src="https://th.bing.com/th/id/R.df339408ea621dbc4329728ce826588b?rik=y72Hh%2bnm%2b%2fxRpA&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fbook-png-transparent%2fbook-png-transparent-25.png&ehk=gmubfvr8FpA4yhyJh4fqvDaB%2bSn6YGmM%2fYOnPJvDYD8%3d&risl=&pid=ImgRaw&r=0">
-        <img src="https://static.wixstatic.com/media/e00768_5a63aff3fdeb4c24b9e7f18e397eb98e~mv2.gif">
-        <h2>
-            <strong>Tutti gli utenti di Reader's List sono in competizione tra loro !</strong> <br>
-            di fatti, ogni lettore ha la possibilità di confrontarsi con tutte le altre persone che nutrono la stessa passione.
-        </h2>
-        
+        <br>
+        <div class="text1">
+            <h2>
+                Creating a Reading List allows you to save all your books and keep track of every reading you are tackling.
+                 Each user has the ability to save all the books they are interested in reading and is able to save their progress
+                 concerning the pages read.
+            </h2>
+        </div>
+
+        <figure>
+            <blockquote>
+                <q>A room without books is like a body without a soul.</q>
+            </blockquote>
+            <figcaption>—Marcus Tullius Cicero</figcaption>
+        </figure>
+
+        <img class="gif" src="https://7551bdfc54adf45425bb-e1819ba959867bdb3382b3652a5f5ff1.ssl.cf5.rackcdn.com/animations/467/1435256441-Young_animation_education062515_02.gif">
+
+        <div class="text2">
+            <h2>
+                <strong>All Reader's List users are competing with each other!</strong>
+                 in fact, each reader has the opportunity to deal with all the other people who have the same passion.
+            </h2>
+        </div>
     </div>
 </body>
 </html>
